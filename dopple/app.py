c96f1423afbe7730074b77be1f8fc0f534991eed
Jeff Rosal <jeros808@aol.com>
2018-10-04 18:27:25 -0500

    add stuff

--- dopple/app.py
+++ dopple/app.py
@@ -1,11 +1,15 @@
+from __future__ import print_function
+import sys
 import os
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, redirect, url_for
 from camera import VideoCamera
-
+buttons = 0
 app = Flask(__name__)
+# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+  	  
     return render_template('index.html')
 
 def gen(camera):
@@ -14,10 +18,30 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    return render_template('data.html')
+
+@app.route('/data')
+def button():
+    print("go to data!")
+    buttons = 1
+    return render_template('data.html')
+# @app.after_request
+# def add_header(response):
+#     response.cache_control.no_store = True
+#     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+#     response.headers['Pragma'] = 'no-cache'
+#     response.headers['Expires'] = '-1'
+#     return response
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+
--- dopple/camera.py
+++ dopple/camera.py
@@ -1,25 +1,31 @@
+import os
 import cv2
 import keyboard
 from sightengine.client import SightengineClient
 import paralleldots
 from paralleldots import set_api_key
+import requests
+import matplotlib.pyplot as plt
+from matplotlib import figure
+import numpy as np
 
 capit = "press SPACEBAR to capture the image"
-exit = "press ESC to EXIT"
 print(capit)
 print(exit)
+
 class VideoCamera(object):
     def get_frame(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
         
-        
+        from app import buttons
         self.video = cv2.VideoCapture(0)
 
         img_counter = 0
         
         while True:
+           
             ret, frame = self.video.read()
             #cv2.imshow("test", frame)
             if not ret:
@@ -32,23 +38,67 @@ class VideoCamera(object):
                 break
             elif keyboard.is_pressed('space'):
         # SPACE pressed
-                img_name = "opencv_frame_{}.png".format(img_counter)
-                cv2.imwrite(img_name, frame)
+                path_img = "/Users/jeffrosal1/Desktop/NUhomework/project3/Project3/dopple/static/img/"
+                img_name = "dopplegang.png"
+                #cv2.imwrite(img_name, frame)
+                
+                cv2.imwrite(str(path_img) + 'dopplegang.png', frame)
                 print("{} written!".format(img_name))
 
                 client = SightengineClient('523702522', 'SoMh4T2mBCTB848RmhqS')
-                output = client.check('celebrities').set_file('/Users/jeffrosal1/Desktop/NUhomework/project3/combined/flasktest/opencv_frame_0.png')
+                output = client.check('celebrities').set_file('/Users/jeffrosal1/Desktop/NUhomework/project3/Project3/dopple/static/img/dopplegang.png')
                 print(output)
                 set_api_key("i0bqh0wRTlMqHMTHXZPxXFumRAcETw698GaIqBN9vuM")
 
 # when sending a image file
 
-                path = "/Users/jeffrosal1/Desktop/NUhomework/project3/combined/flasktest/opencv_frame_0.png"
+                path = "/Users/jeffrosal1/Desktop/NUhomework/project3/Project3/dopple/static/img/dopplegang.png"
                 emoout = paralleldots.facial_emotion(path)         
                 print(emoout)
+
+                #start matplotlib
+                faces = output["faces"]
+                celebrity = faces[0]
+                name_set= celebrity["celebrity"]
+                index = 0
+                totalprob = 0
+                names = []
+                probability = []
+                while index < len(name_set):
+                    x = name_set[index]
+                    name = x['name']
+                    names.append(name)
+
+                    prob = x['prob']
+                    probability.append(prob)
+    
+                    index = index + 1
+                print(names)
+                print(probability)
+                from matplotlib import rcParams
+                rcParams.update({'figure.autolayout': True})
+                plt.bar(names, probability, color='yellow', alpha=0.5, align="center", edgecolor = "black")
+                plt.xticks(rotation=45)
+                plt.ylim(0, 1)
+                plt.title("Celebrity Doppelganger")
+                plt.xlabel("Celebrity Name")
+                plt.ylabel("Probability")                             
+                
+                plt.savefig(str(path_img) + "prob_bar_graph.png")
+                plt.close()
+                #end matplotlib
+
+
+
+
+
+
+
+
             rete, jpeg = cv2.imencode('.jpg', frame)
             return jpeg.tobytes()
 
+     
     def __del__(self):
         self.video.release()
 
